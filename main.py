72a96bb07c2a0d8f611ddc0dc3f6d5a9b4a1085a
MkGee <matthewrmcgeehan2@gmail.com>
2024-08-27 11:47:47 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Hello, World!")
+
+x = 5 
+y = 3.14
+name = "Alice"
+is_student = True
+
+print (x)
+print (name)
+
+if x > 0:
+  print("x is positive")
+else:
+  print("x is negative")
+  
+  
+for i in range(5):
+  print(i)
+  
+count = 0
+while count < 5:
+  print(count)
+  count += 4
